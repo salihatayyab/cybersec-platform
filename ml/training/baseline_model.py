6c0acd63865d169727362a2193f1535c5ff79e11
salihatayyab <salihatayyab188@gmail.com>
2026-04-28 15:48:29 +0500

    Create baseline_model.py

---
+++ ml/training/baseline_model.py
@@ -0,0 +1,29 @@
+# ~/cybersec-platform/ml/training/baseline_model.py
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import joblib
+
+df = pd.read_csv('security_dataset.csv')
+
+features = ['hour_of_day','files_accessed','login_attempts',
+            'foreign_ip','unknown_device','data_uploaded_mb']
+
+X = df[features]
+y = df['is_threat']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+# Model save karo
+joblib.dump(model, '../models/rf_model.pkl')
+print("Model saved!")
